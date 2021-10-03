9b4ddc8679bfdf6bb4f7e266c6c04d40e77682da
nitesh <ergupta.nitesh@hotmail.com>
2021-10-03 21:31:27 +0530

    business desgin complete

--- app/src/main/java/com/e_wayalerts/MainActivity.java
+++ app/src/main/java/com/e_wayalerts/MainActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.e_wayalerts.Utility.Utility;
+import com.e_wayalerts.fragment.BusinessListFragment;
+
 public class MainActivity extends AppCompatActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Utility.loadFragment(MainActivity.this, new BusinessListFragment(),
+				false, null);
 	}
 }
---
+++ app/src/main/java/com/e_wayalerts/Utility/FontAwesome.java
@@ -0,0 +1,35 @@
+package com.e_wayalerts.Utility;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+
+public class FontAwesome extends TextView {
+
+
+    public FontAwesome(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public FontAwesome(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public FontAwesome(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+
+        //Font name should not contain "/".
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(),
+                "fontawesome-webfont.ttf");
+        setTypeface(tf);
+    }
+
+}
